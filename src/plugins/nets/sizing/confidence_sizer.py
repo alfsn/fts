fa2e93091843f056ee3f19f87a138bec99c8fec1
Alfred <alfredosampronnoel@gmail.com>
2026-06-15 17:40:38 -0300

    unsize FLAT signals

--- src/plugins/nets/sizing/confidence_sizer.py
+++ src/plugins/nets/sizing/confidence_sizer.py
@@ -1,6 +1,6 @@
 # src/plugins/nets/sizing/confidence_sizer.py
 
-from trading_bot.core.enums import SizingStrategyType
+from trading_bot.core.enums import SignalType, SizingStrategyType
 from trading_bot.core.schemas import SizingInput, SizingOutput
 from trading_bot.risk_management.abc import BaseSizingStrategy
 
@@ -20,6 +20,9 @@ class ConfidenceSizer(BaseSizingStrategy):
         return SizingStrategyType.FIXED_AMOUNT
 
     def calculate_size(self, input_data: SizingInput) -> SizingOutput:
+        if input_data.signal.signal_type not in (SignalType.BUY, SignalType.SELL):
+            return SizingOutput(amount_quote=0.0, size_shares=0.0)
+
         confidence = input_data.signal.confidence
 
         # Linear scaling: 0% confidence -> 0 size, 100% confidence -> base_amount
