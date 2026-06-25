8cd3d5985f625f55ac896ece4623787897474d2b
Alfred <alfredosampronnoel@gmail.com>
2026-06-25 19:23:02 -0300

    delete BacktestSimulator (use HistoricalLoop)

--- src/trading_bot/backtesting/__init__.py
+++ src/trading_bot/backtesting/__init__.py
@@ -1,9 +1,7 @@
 from .exporter import BaseBacktestExporter, HTMLBacktestExporter
-from .simulator import BacktestSimulator
 from .visualizer import BacktestVisualizer
 
 __all__ = [
-    "BacktestSimulator",
     "BacktestVisualizer",
     "BaseBacktestExporter",
     "HTMLBacktestExporter",
